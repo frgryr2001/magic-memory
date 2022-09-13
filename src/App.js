013080a4b2287b3b856b19c46246ca30ed15e1ae
nhân <nhanle.lx@gmail.com>
2022-09-13 16:48:21 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import "./App.css";
+
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+  console.log(cards, turns);
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div className="card" key={card.id}>
+            <img src={card.src} alt="card font" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
